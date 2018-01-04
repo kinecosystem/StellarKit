c74e10ef172cd99ef7713db1ca48ca8042fc60fd
ashevin <avi.shevin@kik.com>
2018-01-04 07:27:56 +0200

    Initial Commit

---
+++ SwiftyStellar/SwiftyStellar.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyStellar.h
+//  SwiftyStellar
+//
+//  Created by Avi Shevin on 04/01/2018.
+//  Copyright © 2018 Kin Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyStellar.
+FOUNDATION_EXPORT double SwiftyStellarVersionNumber;
+
+//! Project version string for SwiftyStellar.
+FOUNDATION_EXPORT const unsigned char SwiftyStellarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyStellar/PublicHeader.h>
+
+
