9bbb86cd4d398387b20497c986b7eb3e69ddd0f2
ashevin <avi.shevin@kik.com>
2018-10-15 14:09:09 +0300

    Add factory method for MANAGE_DATA ops.

--- StellarKit/source/StellarOps.swift
+++ StellarKit/source/StellarOps.swift
@@ -51,4 +51,14 @@ extension Operation {
         return Operation(sourceAccount: sourcePK,
                          body: Operation.Body.CHANGE_TRUST(ChangeTrustOp(asset: asset)))
     }
+
+    public static func manageData(key: String, value: Data?, source: Account? = nil) -> Operation {
+        var sourcePK: PublicKey? = nil
+        if let source = source, let pk = source.publicKey {
+            sourcePK = PublicKey.PUBLIC_KEY_TYPE_ED25519(WD32(KeyUtils.key(base32: pk)))
+        }
+
+        return Operation(sourceAccount: sourcePK,
+                         body: Operation.Body.MANAGE_DATA(ManageDataOp(dataName: key, dataValue: value)))
+    }
 }
