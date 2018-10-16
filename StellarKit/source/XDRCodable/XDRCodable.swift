faa1b4aca9899cd8a638bfd1db789bc34d5e18e0
ashevin <avi.shevin@kik.com>
2018-10-16 11:01:04 +0300

    Fix bug in Data encoding.

--- StellarKit/source/XDRCodable/XDRCodable.swift
+++ StellarKit/source/XDRCodable/XDRCodable.swift
@@ -198,6 +198,7 @@ extension Data: XDRCodable {
     public func encode(to encoder: XDREncoder) throws {
         try encoder.encode(Int32(count))
         encoder.append(self)
+        encoder.append(Array<UInt8>(repeating: 0, count: (4 - Int(count) % 4) % 4))
     }
 }
 
